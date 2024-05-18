a9a5cc6d426895c6fe9a350eda39d21c5a38e988
Maksimov86 <115940258+Maksimov86@users.noreply.github.com>
2024-05-18 13:25:52 +0700

    Update Location.js

--- src/components/Location.js
+++ src/components/Location.js
@@ -2,6 +2,27 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Location = () => {
+    const { dispath } = useContext(AppContext);
+
+    const changeLocation = (val) => {
+        dispath({
+            type: 'CHG_LOCATION',
+            payload: val,
+        })
+    }
+
+    return (
+        <div className='alert alert-secondary'>Location {
+            <select name='Location' id='Location' onChange={event=>changeLocation(event.target.value)}>
+                <option value='£'>Uk(£)</option>
+                <option value='₹'>India(₹)</option>
+                <option value='€'>Europe(€)</option>
+                <option value='CAD'>Canada(CAD)</option>
+                <option value='₽'>Russia(₽)</option>
+            </select>
+        }
+        </div>
+    );
 };
 
 export default Location;
